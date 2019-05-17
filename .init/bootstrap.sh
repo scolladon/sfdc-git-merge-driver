f7d68b456614ae8bf5f1a407d8c624ec15a42da9
Sebastien <colladonsebastien@gmail.com>
2019-05-17 18:26:20 +0200

    Add permission set support

--- .init/bootstrap.sh
+++ .init/bootstrap.sh
@@ -4,13 +4,13 @@
 type groovy >/dev/null 2>&1 || { echo >&2 "Install groovy first: http://groovy-lang.org/documentation.html.  Aborting."; exit 1; }
 type smp >/dev/null 2>&1 || { echo >&2 "Install smp first (npm install -g sfdc-merge-package): https://github.com/scolladon/sfdc-merge-package.  Aborting."; exit 1; }
 echo "#bootstrap: requirement check done"
-curl -Ls https://github.com/amguerrero/sfdc_merge/tarball/43f2 > sfdc_merge.43f2.tar
-mkdir sfdc_merge.43f2
-tar -xf sfdc_merge.43f2.tar -C sfdc_merge.43f2 --strip-components 1
+curl -Ls https://github.com/KevinGossentCap/sfdc_merge/tarball/bcbb426 > sfdc_merge.latest.tar
+mkdir sfdc_merge.latest
+tar -xf sfdc_merge.latest.tar -C sfdc_merge.latest --strip-components 1
 echo "#bootstrap: download & install dependencies done"
 mkdir ../.git/scripts
-cp -R sfdc_merge.43f2/ ../.git/scripts/sf_merge
-rm -rf sfdc_merge.43f2 sfdc_merge.43f2.tar
+cp -R sfdc_merge.latest/ ../.git/scripts/sf_merge
+rm -rf sfdc_merge.latest sfdc_merge.latest.tar
 cat gitconf >> ../.git/config
 echo "#bootstrap: install custom driver in git config done"
 mkdir ../.git/info
